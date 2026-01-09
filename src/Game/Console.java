76377c9b0ccb83b6df467b494bf87d043648fd4b
Maktis <pospisilmatysek@gmail.com>
2026-01-09 17:37:42 +0100

    Finish Console programming preparation

--- src/Game/Console.java
+++ src/Game/Console.java
@@ -1,5 +1,16 @@
 package Game;
 
 public class Console {
+    private Initialization init;
 
+
+    public void setup(){
+        //TODO setup metoda chybi
+    }
+
+    public void execute(){
+        //TODO execute metoda chybi
+    }
+
+    //Další metody si přidám podle potřeby
 }
