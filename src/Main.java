ae4c636dfc29dbffd1729d456ac1806a20a6b777
Maktis <pospisilmatysek@gmail.com>
2026-01-16 19:42:14 +0100

    All locations including main/hallway/side can be loaded separately Connection in between then has to be done via CSV file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import Locations.Location;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.FileInputStream;
@@ -15,8 +14,14 @@ public class Main {
         ArrayList<Location> locations = new ArrayList<>();
         try {
             InputStream input = new FileInputStream("res\\locations.json");
+            InputStream input2 = new FileInputStream("res\\hallwayLocations.json");
+            InputStream input3 = new FileInputStream("res\\sideLocations.json");
             Location[] location = mapper.readValue(input, Location[].class);
+            Location[] location2 = mapper.readValue(input2, Location[].class);
+            Location[] location3 = mapper.readValue(input3, Location[].class);
             locations.addAll(List.of(location));
+            locations.addAll(List.of(location2));
+            locations.addAll(List.of(location3));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
