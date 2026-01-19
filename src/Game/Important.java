add3b13f923e1ce3163d9ff3ece04ff6d2392105
Maktis <pospisilmatysek@gmail.com>
2026-01-19 13:15:33 +0100

    Add colour system. Thank you Tonda for help:]

---
+++ src/Game/Important.java
@@ -0,0 +1,30 @@
+package Game;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Important {
+
+
+    public static String colourMap(String colour) {
+        HashMap<String, String> colourMap = loadColourMap("res\\colours.csv");
+        return colourMap.get(colour);
+    }
+
+    private static HashMap<String, String> loadColourMap(String pathName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(pathName))) {
+            HashMap<String, String> temp = new HashMap<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(">");
+                temp.put(data[0], data[1]);
+            }
+            return temp;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        testLoader3();
+        testLocationMovement1();
 
 
     }
