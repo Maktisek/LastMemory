ac60e66e293dabed0dfc1dbc250e514ddbf46a92
Maktis <pospisilmatysek@gmail.com>
2026-01-23 22:26:15 +0100

    Add guard clauses into AcceptTaskCommand

--- src/Commands/AcceptTaskCommand.java
+++ src/Commands/AcceptTaskCommand.java
@@ -2,6 +2,7 @@ package Commands;
 
 import AroundPlayer.Player;
 import Items.Task;
+import NPCS.FriendlyNPC;
 
 /**
  * Command designed to accept task from current location friendlyNPC
@@ -16,21 +17,17 @@ public class AcceptTaskCommand implements Command{
 
     @Override
     public String execute() {
-        if(player.getCurrentLocation().getFriendlyNPC() != null){
-            Task temp = player.getCurrentLocation().getFriendlyNPC().getTask();
-            if (temp != null){
-                if(player.addCurrentTask(temp)){
-                    return "Nový úkol " + temp.getName() + " byl přijat";
-                }else {
-                    return "Nyní nemůžeš přijmat žádné úkoly";
-                }
-            }else {
-                return player.getCurrentLocation().getFriendlyNPC().getName() + " již žádný úkol nenabízí";
-            }
-        }else {
-            return "V lokaci se nyní nikdo nenachází ";
+        FriendlyNPC friendlyNPC = player.getCurrentLocation().getFriendlyNPC();
+        if(friendlyNPC == null){
+            return "V lokaci se nyní nikdo nenachází";
         }
-
+        if(friendlyNPC.getTask() == null){
+            return friendlyNPC.getName() + " již žádný úkol nenabízí";
+        }
+        if (player.addCurrentTask(friendlyNPC.getTask())){
+            return "Nový úkol " + friendlyNPC.getTask().getName() + " byl přijat";
+        }
+        return "Nyní nemůžeš přijmat žádné úkoly";
     }
 
     @Override
