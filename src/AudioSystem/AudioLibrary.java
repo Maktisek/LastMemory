7153a547961e030cc948ddfe934b22546ad60001
Maktis <pospisilmatysek@gmail.com>
2026-01-24 15:23:24 +0100

    Start working on audioSystem.

---
+++ src/AudioSystem/AudioLibrary.java
@@ -0,0 +1,16 @@
+package AudioSystem;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
+public class AudioLibrary {
+
+     private ArrayList<Audio> audios;
+     private ObjectMapper mapper;
+
+
+
+
+
+}
