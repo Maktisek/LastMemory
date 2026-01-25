9517f95670fe9d8f174882617df6f966d8df82b6
Maktis <pospisilmatysek@gmail.com>
2026-01-25 13:43:25 +0100

    Prepare Cutscene for further programming

--- src/Cutscenes/Cutscene.java
+++ src/Cutscenes/Cutscene.java
@@ -1,4 +1,40 @@
 package Cutscenes;
 
 public class Cutscene {
+
+    private String name;
+    private String scene;
+    private int requiredMemories;
+
+    public Cutscene() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScene() {
+        return scene;
+    }
+
+    public void setScene(String scene) {
+        this.scene = scene;
+    }
+
+    public int getRequiredMemories() {
+        return requiredMemories;
+    }
+
+    public void setRequiredMemories(int requiredMemories) {
+        this.requiredMemories = requiredMemories;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + scene;
+    }
 }
