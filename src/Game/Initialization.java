1e3fc4e71abb6a7e1b572079e38d6f23115c8ff6
Maktis <pospisilmatysek@gmail.com>
2026-02-03 20:43:24 +0100

    add music into location: Ředitelna

--- src/Game/Initialization.java
+++ src/Game/Initialization.java
@@ -140,7 +140,7 @@ public class Initialization {
 
     public void loadPlayer() {
         //11 je startovní lokace
-        this.player = new Player(locations.get(22));
+        this.player = new Player(locations.get(24));
     }
 
     public Audio[] loadAllSongs() throws WrongInitializationException {
