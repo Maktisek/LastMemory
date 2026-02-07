d71c66476435877ef354ab2fbe54db4896516754
Maktis <pospisilmatysek@gmail.com>
2026-02-07 20:49:04 +0100

    add documentation for HandInCommand.java

--- src/Commands/Tasks/HandInCommand.java
+++ src/Commands/Tasks/HandInCommand.java
@@ -4,6 +4,22 @@ import AroundPlayer.Player;
 import Commands.Command;
 import Game.Important;
 
+/**
+ * Represents a command, which hands in all possible items to {@link NPCS.FriendlyNPC}.
+ * <p>
+ *     If the player has no task accepted, no friendly NPC is currently in player’s current location,
+ *     the friendly NPC has no task or the player’s task does not match with friendly NPC’s task,
+ *     then no action is performed, {@link #continues} is set to false
+ *     and a message is returned indicating that there is no one to hand items to.
+ * </p>
+ * If player has no items, which could be handed in, then no action is performed, {@link #continues} is set to false
+ * and a message is returned indicating that there is nothing to be handed in.
+ * <p>
+ *     Otherwise, all items that can be handed in are handed over and a message is returned
+ *     indicating which items have been handed in.
+ * </p>
+ * @author Matěj Pospíšil
+ */
 public class HandInCommand implements Command {
 
     private final Player player;
