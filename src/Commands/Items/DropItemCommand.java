f795bf786b7bb5c663a305a06764884ebb4cab4e
Maktis <pospisilmatysek@gmail.com>
2026-02-07 18:36:49 +0100

    add documentation for DropItemCommand.java

--- src/Commands/Items/DropItemCommand.java
+++ src/Commands/Items/DropItemCommand.java
@@ -4,6 +4,15 @@ import AroundPlayer.Player;
 import Commands.Command;
 import Game.Important;
 
+/**
+ * Represents a command, which drops an item.
+ *  <p>
+ * {@link #name} stands for the name of the item to be dropped.
+ * If the item is not found in player’s inventory, then no action is performed. Only the information about
+ * the process is returned.
+ * Otherwise, the item is dropped.
+ * @author Matěj Pospíšil
+ */
 public class DropItemCommand implements Command {
 
     private final Player player;
@@ -16,17 +25,16 @@ public class DropItemCommand implements Command {
 
     @Override
     public String execute() {
-        if (player.getInventory().getWeight() != 0) {
-            if (player.getCurrentLocation().addItem(player.getInventory().dropItem(name))) {
-                Important.playSound("dropping sound");
-                return "Položil si " + name + " do " + player.getCurrentLocation().getName();
-            }
-            Important.playSound("wrong sound");
-            return Important.changeText("red", "Momentálně u sebe nemáš " + name);
-        }else {
+        if (player.getInventory().getWeight() == 0){
             Important.playSound("wrong sound");
             return Important.changeText("red", "Momentálně u sebe nemáš ani jeden předmět");
         }
+        if (!player.getCurrentLocation().addItem(player.getInventory().dropItem(name))){
+            Important.playSound("wrong sound");
+            return Important.changeText("red", "Momentálně u sebe nemáš " + name);
+        }
+        Important.playSound("dropping sound");
+        return "Položil si " + name + " do " + player.getCurrentLocation().getName();
     }
 
     @Override
