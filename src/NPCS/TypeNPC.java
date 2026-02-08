c686e59a0ab10f604f14076425fc685bbf7d8633
Maktis <pospisilmatysek@gmail.com>
2026-02-08 19:50:16 +0100

    finish documentation for TypeNPC.java

--- src/NPCS/TypeNPC.java
+++ src/NPCS/TypeNPC.java
@@ -1,5 +1,16 @@
 package NPCS;
 
+/**
+ * Represents the type of {@link FriendlyNPC}.
+ * @author Matěj Pospíšil
+ */
 public enum TypeNPC {
-    AKTIVNÍ, PASIVNÍ
+    /**
+     * Represents a {@link FriendlyNPC} who has a task and can give a memory to the player.
+      */
+    AKTIVNÍ,
+    /**
+     * Represents a {@link FriendlyNPC} who does not have a task and cannot give a memory to the player.
+     */
+    PASIVNÍ
 }
