250910600baafa9ca48dbb67b11d0d4bcbe5c007
Maktis <pospisilmatysek@gmail.com>
2026-02-08 18:57:11 +0100

    finish documentation for Task.java

--- src/Items/Task.java
+++ src/Items/Task.java
@@ -8,6 +8,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Represents a task in the game.
+ * <p>
+ * Each task has items that the player must hand over.
+ * Items are stored not as {@link Item} instances, but as names and codes
+ * in {@link #namesOfNeededObjects} and {@link #codesOfNeededObjects}.
+ * </p>
+ * <p>
+ * Each task also has a reward, represented by a {@link Memory}.
+ * </p>
+ *
+ * @author Matěj Pospíšil
+ */
 public class Task {
 
     private String name;
@@ -20,6 +33,12 @@ public class Task {
     public Task() {
     }
 
+    /**
+     * Checks if the player has any of the items required to solve the task.
+     *
+     * @param player the player whose inventory will be checked
+     * @return true if the player has at least one required item, false otherwise
+     */
     public boolean canSolve(Player player) {
         for (String key : player.getInventory().getItems().keySet()) {
             if (codesOfNeededObjects.contains(key)) {
@@ -30,6 +49,22 @@ public class Task {
         return false;
     }
 
+    /**
+     * Processes a task by handing over items from the player's inventory.
+     * <p>
+     * Iterates through the player's inventory keys and checks for matches with
+     * {@link #codesOfNeededObjects}. If a match is found, the corresponding item’s name
+     * and its code are removed from  {@link #namesOfNeededObjects} and {@link #codesOfNeededObjects}
+     * </p>
+     * <p>
+     * All removed items are stored in a temporary {@code deletedItems} list,
+     * which is then removed from the player's inventory.
+     * </p>
+     *
+     * @param player the player whose inventory will be scanned for required items
+     * @return a formatted String listing the items that were handed over,
+     *         or an empty string if no items were handed over
+     */
     public String scanAndSolveTask(Player player) {
         ArrayList<String> temp = new ArrayList<>();
         ArrayList<Item> deletedItems = new ArrayList<>();
@@ -65,6 +100,10 @@ public class Task {
         return Important.writeStringArrays(namesOfNeededObjects);
     }
 
+    /**
+     * Prepares the task to be written through {@link Important#dashToString(String, String)}
+     * @return the toString to be written through {@link Important#dashToString(String, String)}
+     */
     public String forToString() {
         return this.description + "\n" +
                 Important.changeText("bold", "Zbývající potřebné předměty k odevzdání: ") + writeAllNeededItems();
