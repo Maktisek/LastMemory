318287c6d11c399f43379886ca1d05a406fb4d8b
Maktis <pospisilmatysek@gmail.com>
2026-02-08 19:22:06 +0100

    finish documentation for Type.java

--- src/Locations/Type.java
+++ src/Locations/Type.java
@@ -1,6 +1,21 @@
 package Locations;
 
+/**
+ * Represents the type of location.
+ * @author Matěj Pospíšil
+ */
 public enum Type {
+    /**
+     * Represents a normal location.
+     */
+    LOCATION,
+    /**
+     * Represents a hallway location.
+     */
+    HALLWAY,
 
-    HALLWAY, LOCATION, FADE
+    /**
+     * Represents a special location, which has to have a neighboring location with {@link Type#FADE} too in order to work properly.
+     */
+    FADE
 }
