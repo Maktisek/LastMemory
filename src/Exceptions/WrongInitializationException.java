6e7ae2423b667a3280fe370a8c8c89a8061a16d8
Maktis <pospisilmatysek@gmail.com>
2026-02-08 14:26:14 +0100

    add documentation for WrongInitializationException.java and WrongSafeCodeException.java

--- src/Exceptions/WrongInitializationException.java
+++ src/Exceptions/WrongInitializationException.java
@@ -1,5 +1,8 @@
 package Exceptions;
-
+/**
+ * Thrown when the game or one of its components is initialized incorrectly.
+ * @author Matěj Pospíšil
+ */
 public class WrongInitializationException extends Exception {
     public WrongInitializationException(String message) {
         super(message);
--- src/Exceptions/WrongSafeCodeException.java
+++ src/Exceptions/WrongSafeCodeException.java
@@ -1,5 +1,8 @@
 package Exceptions;
-
+/**
+ * Thrown when the code of the safe is inserted incorrectly.
+ * @author Matěj Pospíšil
+ */
 public class WrongSafeCodeException extends Exception {
     public WrongSafeCodeException(String message) {
         super(message);
