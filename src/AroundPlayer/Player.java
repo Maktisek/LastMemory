01afcd530f976372f861cf0aaefa4c8a16a3b1e7
Maktis <pospisilmatysek@gmail.com>
2026-02-18 22:26:14 +0100

    now the game is ready

--- src/AroundPlayer/Player.java
+++ src/AroundPlayer/Player.java
@@ -182,7 +182,7 @@ public class Player {
     }
 
     public boolean canEnd() {
-        return collectedMemories.size() == 1;
+        return collectedMemories.size() == 10;
     }
 
     public void switchMode(Mode mode) {
