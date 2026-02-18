15e00d5bee5031a012495109f390cfee5651de07
Maktis <pospisilmatysek@gmail.com>
2026-02-18 22:25:55 +0100

    improve the pre end sentence

--- src/Commands/TasksCommands/EvaluateTaskCommand.java
+++ src/Commands/TasksCommands/EvaluateTaskCommand.java
@@ -38,6 +38,9 @@ public class EvaluateTaskCommand implements Command {
         player.getCurrentLocation().getFriendlyNPC().switchSpeeches();
         player.getCurrentLocation().pauseMusic();
         Important.playSound("new memory");
+        if(player.canEnd()){
+            return "Ou, já už to chápu...";
+        }
         return "Ou, myslím, že si na něco vzpomínám...";
     }
 
