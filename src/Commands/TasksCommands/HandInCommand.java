22597d92cef83c102db4056b4e105a1c7aa4edae
Maktis <pospisilmatysek@gmail.com>
2026-02-22 16:20:42 +0100

    improve HandInCommand

--- src/Commands/TasksCommands/HandInCommand.java
+++ src/Commands/TasksCommands/HandInCommand.java
@@ -24,10 +24,14 @@ public class HandInCommand implements Command {
 
     private final Player player;
     private boolean continues;
+    private boolean isWaitAble;
+    private boolean isTimeWaitAble;
 
     public HandInCommand(Player player) {
         this.player = player;
         this.continues = true;
+        this.isWaitAble = true;
+        this.isTimeWaitAble = true;
     }
 
     @Override
@@ -35,26 +39,36 @@ public class HandInCommand implements Command {
         if(player.getCurrentTask() == null){
             Important.playSound("wrong sound");
             continues = false;
+            isWaitAble = false;
+            isTimeWaitAble = false;
             return Important.writeSpace(60)+Important.changeText("red", "Momentálně nemáš žádný aktivní úkol");
         }
         if(player.getCurrentLocation().getFriendlyNPC() == null){
             Important.playSound("wrong sound");
             continues = false;
+            isWaitAble = false;
+            isTimeWaitAble = false;
             return Important.writeSpace(60)+Important.changeText("red", "Tady není komu odevzdávat předměty");
         }
         if(player.getCurrentLocation().getFriendlyNPC().getTask() == null){
             Important.playSound("wrong sound");
             continues = false;
+            isWaitAble = false;
+            isTimeWaitAble = false;
             return Important.writeSpace(60)+Important.changeText("red", "U "+player.getCurrentLocation().getFriendlyNPC().getName() + " nemáš žádný aktivní úkol");
         }
         if(!player.getCurrentTask().getName().equalsIgnoreCase(player.getCurrentLocation().getFriendlyNPC().getTask().getName())){
             Important.playSound("wrong sound");
             continues = false;
+            isWaitAble = false;
+            isTimeWaitAble = false;
             return Important.writeSpace(60)+Important.changeText("red", player.getCurrentLocation().getFriendlyNPC().getName() + " ti nezadal " + player.getCurrentTask().getName());
         }
         if(!player.getCurrentTask().canSolve(player)){
             Important.playSound("wrong sound");
             continues = false;
+            isWaitAble = false;
+            isTimeWaitAble = false;
             return Important.writeSpace(60)+Important.changeText("red", "Nelze odevzdat žádný předmět");
         }
         String result = player.getCurrentTask().scanAndSolveTask(player);
@@ -70,7 +84,7 @@ public class HandInCommand implements Command {
 
     @Override
     public boolean isWaitAble() {
-        return true;
+        return isWaitAble;
     }
 
     @Override
